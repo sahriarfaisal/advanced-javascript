308cb99957501ba6451700c3107f7c7adef3d61d
sahriarfaisal.cse@gmail.com <sahriarfaisal.cse@gmail.com>
2020-08-07 16:22:44 +0600

    false vs true

---
+++ true-false.js
@@ -0,0 +1,53 @@
+// const age = 0;// number ta jodi 0 na thake onno jekono number thakto ta hole javascript default true true nai.
+// if(age){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//================================================
+
+// "" Empty string is return falsey value otherwise return truthy
+// const name = "";
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+
+//=================================================
+//When you don't set a value in a variable then its return undefined
+//Undefined allows return false. 
+// let name; //Undefined variable.
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//===================================================
+
+// let name = null; //null defined variable return false.
+// console.log(name);
+// if(name){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//=====================================================
+
+let name = NaN; //NaN defined variable return false.
+console.log(name);
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
